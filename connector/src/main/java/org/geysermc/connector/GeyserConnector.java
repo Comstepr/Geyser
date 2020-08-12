d53e4c034aab3db92ff4aa89ce21eee16efe19fa
Comstepr <32700815+Comstepr@users.noreply.github.com>
2020-08-12 18:48:48 +0800

    Update GeyserConnector.java

--- connector/src/main/java/org/geysermc/connector/GeyserConnector.java
+++ connector/src/main/java/org/geysermc/connector/GeyserConnector.java
@@ -52,7 +52,6 @@ import org.geysermc.connector.network.translators.sound.SoundRegistry;
 import org.geysermc.connector.network.translators.world.WorldManager;
 import org.geysermc.connector.network.translators.world.block.BlockTranslator;
 import org.geysermc.connector.network.translators.world.block.entity.BlockEntityTranslator;
-import org.geysermc.connector.network.translators.world.collision.CollisionTranslator;
 import org.geysermc.connector.utils.DimensionUtils;
 import org.geysermc.connector.utils.DockerCheck;
 import org.geysermc.connector.utils.LanguageUtils;
